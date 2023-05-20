559aa520cf057df7f84088e7dba2103be852f92d
LeonardYam <yamthesmall@gmail.com>
2023-05-20 16:51:19 +0800

    Add web analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Inter } from 'next/font/google'
+import { Analytics } from "@vercel/analytics/react";
 import React from "react";
 import NavBar from "@/app/NavBar";
 import Footer from "@/app/Footer";
@@ -25,6 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Analytics/>
         <NavBar/>
         <main>
           {children}
