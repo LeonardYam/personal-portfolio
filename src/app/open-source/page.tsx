7dda7fd0896038b21f0ecc29977f89239ec709b1
LeonardYam <yamthesmall@gmail.com>
2023-08-07 00:31:08 +0800

    docs: update open-source information

--- src/app/open-source/page.tsx
+++ src/app/open-source/page.tsx
@@ -1,8 +1,15 @@
 export default function Page() {
 	return (
-		<p>
-			I currently contribute to <a href={"https://github.com/web-platform-tests/wpt.fyi"}>wpt.fyi</a>,
-			a dashboard for web-platform-tests which is used by major browser developers.
-		</p>
+		<>
+			<p>
+				I currently contribute to <a href={"https://github.com/opengovsg/FormSG"}>FormSG</a>,
+				a form builder application built,
+				open sourced and maintained by <a href={"https://www.open.gov.sg/"}>Open Government Products</a>.
+			</p>
+			<p>
+				I previously contributed to <a href={"https://github.com/web-platform-tests/wpt.fyi"}>wpt.fyi</a>,
+				a dashboard for web-platform-tests which is used by major browser developers.
+			</p>
+		</>
 	)
 }
